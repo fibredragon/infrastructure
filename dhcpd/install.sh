098ec424888371f930b7c3588e97858aa087dca4
Daniel Hyders <hello@bean.codes>
2021-09-27 05:13:22 +0100

    Push change with new installer content

--- dhcpd/install.sh
+++ dhcpd/install.sh
@@ -4,4 +4,5 @@ rm -rf /etc/dhcp/scripts/
 mkdir -p /etc/dhcp/scripts/
 
 curl -L -o /etc/dhcp/scripts/dhcpd.conf https://raw.githubusercontent.com/fibredragon/infrastructure/master/dhcpd/scripts/dhcpd.conf
-curl -L -o /etc/dhcp/scripts/start-dhcpd.sh https://raw.githubusercontent.com/fibredragon/infrastructure/master/dhcpd/scripts/start-dhcpd.sh
+curl -L -o /usr/bin/dhcpd.sh https://raw.githubusercontent.com/fibredragon/infrastructure/master/dhcpd/scripts/start-dhcpd.sh
+curl -L -o /etc/systemd/system/dhcpd.service https://raw.githubusercontent.com/fibredragon/infrastructure/master/dhcpd/systemd.service
