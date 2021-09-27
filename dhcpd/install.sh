e9a956abf0e190378a2a38df10efcf37517a815e
Daniel Hyders <hello@bean.codes>
2021-09-27 05:20:01 +0100

    Complete installer

--- dhcpd/install.sh
+++ dhcpd/install.sh
@@ -4,5 +4,9 @@ rm -rf /etc/dhcp/scripts/
 mkdir -p /etc/dhcp/scripts/
 
 curl -L -o /etc/dhcp/scripts/dhcpd.conf https://raw.githubusercontent.com/fibredragon/infrastructure/master/dhcpd/scripts/dhcpd.conf
+curl -L -o /etc/dhcp/scripts/multiconf.sh https://raw.githubusercontent.com/fibredragon/infrastructure/master/dhcpd/scripts/multiconf.sh
 curl -L -o /usr/bin/start-dhcpd.sh https://raw.githubusercontent.com/fibredragon/infrastructure/master/dhcpd/scripts/start-dhcpd.sh
 curl -L -o /etc/systemd/system/dhcpd.service https://raw.githubusercontent.com/fibredragon/infrastructure/master/dhcpd/systemd.service
+
+chmod +x /usr/bin/start-dhcpd.sh
+chmod +x /etc/dhcp/scripts/multiconf.sh
