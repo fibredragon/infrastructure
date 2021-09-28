7a117d9209e56c4d6cd5f4a9a524a53699625542
Daniel Hyders <hello@bean.codes>
2021-09-28 02:40:00 +0100

    A few changes relating to startup
    - Make startup script reliant on systemd, init.d is unreliable for startup
    
    - Refactor systemd service to start up independently of the DHCP server, after network.target is triggered

--- dhcpd/scripts/start-dhcpd.sh
+++ dhcpd/scripts/start-dhcpd.sh
@@ -2,7 +2,6 @@
 SCRIPT_DIR=/etc/dhcp/scripts 
 "$SCRIPT_DIR"/multiconf.sh
 
-/etc/init.d/isc-dhcp-server stop
 sudo systemctl stop isc-dhcp-server.service
-/etc/init.d/isc-dhcp-server start
+wait $!
 sudo systemctl start isc-dhcp-server.service
